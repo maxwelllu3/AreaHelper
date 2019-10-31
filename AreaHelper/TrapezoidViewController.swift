aa3a4289ff7fbaedb88eff0af6a7e154c6062a90
Maxwell Lu <blu@lcs.on.ca>
2019-10-31 19:45:15 -0400

    Got trapezoid guard let statement working.

--- AreaHelper/TrapezoidViewController.swift
+++ AreaHelper/TrapezoidViewController.swift
@@ -33,12 +33,35 @@ class TrapezoidViewController: UIViewController {
         
     @IBAction func calculate(_ sender: Any) {
         
-        let givenBase1 = base1.text!
-        let givenBase2 = base2.text!
-        let givenHeight = height.text!
-        let numericBase1 = Double(givenBase1)!
-        let numericBase2 = Double(givenBase2)!
-        let numericHeight = Double(givenHeight)!
+        guard let givenBase1 = base1.text, givenBase1 != "" else {
+            out.text = "Required value not found!"
+            return
+        }
+        
+        guard let givenBase2 = base1.text, givenBase2 != "" else {
+            out.text = "Required value not found!"
+            return
+        }
+        
+        guard let givenHeight = height.text, givenHeight != "" else {
+            out.text = "Required value not found!"
+            return
+        }
+        
+        guard let numericBase1 = Double(givenBase1) else {
+            out.text = "Please enter a number!"
+            return
+        }
+        
+        guard let numericBase2 = Double(givenBase2) else {
+            out.text = "Please enter a number!"
+            return
+        }
+        
+        guard let numericHeight = Double(givenHeight) else {
+            out.text = "Please enter a number!"
+            return
+        }
         
         out.text = "Area of Trapezoid: \(areaOfTrapezoid(base1: numericBase1, base2: numericBase2, height: numericHeight))"
         
